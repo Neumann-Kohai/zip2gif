916f745c1ef5781b86a2fa078880088a8b7f8cbe
Neumann-Kohai <contact@neumann-kohai.de>
2021-03-27 18:48:28 +0100

    Added some cl arguments

--- zip2gif/Program.cs
+++ zip2gif/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ShellProgressBar;
 using System;
 using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Drawing;
 using System.IO;
 using System.IO.Compression;
@@ -13,6 +14,7 @@ namespace zip2gif
     internal class Program
     {
         static int delay = 30;
+        static int bitDepth = 8;
         static bool ignore = false;
 
         private static readonly ProgressBarOptions options = new ProgressBarOptions
@@ -35,28 +37,48 @@ namespace zip2gif
 
         private static void Main(string[] unparsedArgs)
         {
+            string path = "";
+            string output = "";
+            bool recursive = false;
             {
                 RootCommand root = new RootCommand("A simple Program to convert zip -> gif");
-                root.AddArgument(new Argument<string>("path", Directory.GetCurrentDirectory, description: "Path to eather the folder containgig zip files or a path to a specific zip file"));
+                root.AddArgument(new Argument<string>("path", getDefaultValue: Directory.GetCurrentDirectory, description: "Path to eather the folder containgig zip files or a path to a specific zip file"));
                 root.AddOption(new Option<bool>(new[] { "-r", "--recursive" }, description: "If set subdirektory are search for zip files"));
                 root.AddOption(new Option<int>(new[] { "-fps", "--framerate" },() => -1, description: "set Framerate"));
                 root.AddOption(new Option<int>(new[] {"--delay"},() => -1, description:"set delay between frames"));
                 root.AddOption(new Option<bool>(new[] { "-i", "--ignore" }, description: "if set animation.json is ignored"));
+                root.AddOption(new Option<string>(new[] { "-o", "--output" }, () => "", description: "set to change output path, default same as zip files"));
+                root.AddOption(new Option<int>(new[] { "-bit" }, () => 8, description: "Collor deapth, Eather 4 or 8 bit"));
 
-                var args = root.Parse(unparsedArgs);
-
-                if (args.FindResultFor())
-                    delay = 1000 / args.FindResultFor("");
-
+                root.Handler = CommandHandler.Create<string, bool, int, int, bool, string, int>((string p, bool r, int f, int d, bool i, string o, int b)=> 
+                {
+                    if (f > 0)
+                        delay = 1000 / f;
+                    else if (d > 0)
+                        delay = d;
+                    ignore = i;
+                    path = Path.GetFullPath(p);
+                    recursive = r;
+                    output = o;
+                    if (b != 4 || b != 8)
+                    {
+                        Console.WriteLine("Invalid Pixel deapth");
+                        return;
+                    }
+                    bitDepth = b / 4;
+                });
+                root.Invoke(unparsedArgs);
             }
 
-        }
-
-        private static void XxX(string path, bool recursive)
-        {
-            path = Path.GetFullPath(path);
-            Console.WriteLine(recursive);
+            Console.WriteLine("Debug");
             Console.WriteLine(path);
+            Console.WriteLine(recursive);
+            Console.WriteLine(delay);
+            Console.WriteLine(ignore);
+            Console.WriteLine(output);
+            Console.WriteLine(bitDepth);
+
+
             ProgressBar pbar = new ProgressBar(1, "Total", options);
             if (File.Exists(path) && path.EndsWith(".zip"))
                 CreateGif((path, pbar, (CountdownEvent)null));
@@ -79,6 +101,7 @@ namespace zip2gif
                     countdown.Wait();
                 }
             }
+
         }
 
         private static void CreateGif(object stateInfo)
